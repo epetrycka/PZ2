20cf287dcad9ed571e8f3f987930d545ca630940
Eliza <elizapetrycka@gmail.com>
2025-03-07 22:12:38 +0100

    First labs

---
+++ lab01/zad1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace zad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int repeat = 0;
+
+            try
+            {
+                if (args.Length == 0)
+                {
+                    throw new ArgumentException("No arguments provided");
+                }
+
+                repeat = int.Parse(args.Last());
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Wrong format error: " + e.Message);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("No arguments provided error: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
+
+            for (int i=0; i<repeat; i++){
+                Console.WriteLine(string.Join(" ", args[..^1]));
+            }
+        }
+    }
+}
---
+++ lab01/zad2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace zad2;
+
+class Program{
+    static void Main(string[] args){
+        Console.WriteLine("Wypisz dane liczbowe");
+        int value = 1;
+        do {
+            try{
+                value = int.Parse(Console.ReadLine());
+            }
+            catch (Exception e){}
+        } while (value!=0);
+    }
+}
