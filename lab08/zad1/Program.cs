5a1a206acb11f59562244def620f34b1c7e0700d
Eliza <elizapetrycka@gmail.com>
2025-04-30 13:41:05 +0200

    Lab08

---
+++ lab08/zad1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.SQLite;
+
+using lab8;
+
+public class Program{
+
+    static void Main(string[] args)
+        {
+            var csv = new ReadCSV("zwierzaki_200.csv", ',');
+            var headers = csv.Headers ?? throw new InvalidOperationException("Brak headera");
+
+            var schema = csv.InferColumnSchema();
+
+            using var conn = new SQLiteConnection("Data Source=zwierzatka.db;");
+            conn.Open();
+
+            SqliteUtils.CreateTable("zwierzatka", headers, schema, conn);
+
+            SqliteUtils.InsertRows("zwierzatka", headers, csv, conn);
+
+            SqliteUtils.PrintTable("zwierzatka", conn);
+        }
+}
