0ec732b71c16e366d8de8be2618a8bcba97441c8
Eliza <elizapetrycka@gmail.com>
2025-06-05 19:36:01 +0200

    Sqlite

--- lab12/zad/obj/Debug/net9.0/zad.AssemblyInfo.cs
+++ lab12/zad/obj/Debug/net9.0/zad.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("zad")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e50bde266ed776d9c6a54cf2a99e4440d4cea6de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+527d0641c0ed46004d689638a503e9338222a03e")]
 [assembly: System.Reflection.AssemblyProductAttribute("zad")]
 [assembly: System.Reflection.AssemblyTitleAttribute("zad")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
